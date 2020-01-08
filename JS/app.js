8f8b449f12b6a0ac8a85530d2044e46a0c242753
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-08 17:11:23 -0500

    Beautified representation of today and added TODO list at the top

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,14 @@
+//Need to make a for loop to add input block on each iteration
+//Add button connected to each input block
+//Have button functional, correspond to each input field
+  //Have button save data to local storage
+
+
+//https://devhints.io/moment
+//https://www.youtube.com/watch?v=I8LdKQHrdUY
+
 //Variable that shows today's date and time:
 let today = new Date();
 //Adding that variable to display in the DOM
-$("#currentDay").text(today);
+$("#currentDay").text(`Today's current date and time: ${today}`);
+
