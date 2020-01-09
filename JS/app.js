4945b5de24f4b8716a139aa75b3d27dd95e37365
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-08 19:51:33 -0500

    Have correct times and input fields added

--- JS/app.js
+++ JS/app.js
@@ -18,13 +18,15 @@ $(document).ready(function(){
   jumbo.attr("class", "jumbotron");
   $(".container").append(jumbo);
 
-  for(let i=0; i<10; i++){
-    let time = 900;
-    time = time + 100;
+  for(let i=0; i<9; i++){
+    let time = ["9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM"]
     let timeDiv = $("<div>");
     timeDiv.attr("class", "hour row time-block");
-    timeDiv.text(time);
+    timeDiv.text(time[i]);
     jumbo.append(timeDiv);
+    let task = $("<input>");
+    task.attr("class", "past present future");
+    timeDiv.append(task);
   }
 
 })
