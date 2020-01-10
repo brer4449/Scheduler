57d0b7c536979915bb43450e4d0354568d76ca34
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-10 10:19:57 -0500

    added moment examples, crucial for hw assignment

--- JS/app.js
+++ JS/app.js
@@ -12,21 +12,14 @@ $("#currentDay").text(`Today's current date and time: ${today}`);
 // let start = "2010-10-15";
 // let result = moment(start).fromNow();
 // console.log(result);
-let now = new Date().getTime();
-m = moment(now).toDate();
-console.log(m);
 $(document).ready(function(){
   let row = $("<div>");
   row.attr("class", "row");
   $(".container").append(row);
-  let start = new Date().getHours(8);
-  // if(today<start){
-  //   console.log("it's earlier than 9AM!")
-  // } else {
-  //   console.log("it's later than 9AM!")
-  // }
-  // console.log(Array.from(today));
-  // console.log(typeof start);
+  let currentHour = moment().format("h");
+  let setHour = moment().set("hour", 12);
+  console.log(setHour);
+  console.log(currentHour);
   for(let i=0; i<9; i++){
     let time = ["9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM"]
     let timeDiv = $("<div>");
@@ -34,7 +27,8 @@ $(document).ready(function(){
     timeDiv.text(time[i]);
     row.append(timeDiv);
     let input = $("<input>");
-    input.attr("class", "past future present col-6");
+    input.attr("class", "col-6");
+    
     row.append(input);
     let saveBtn = $("<button>");
     saveBtn.attr("class", "saveBtn col-3");
