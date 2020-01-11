62137af98b2bd07639d204101cd10463dd02c9f5
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-10 19:39:12 -0500

    added correct color to input fields!

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,7 @@
 //Need to make a for loop to add input block on each iteration✓
 //Add button connected to each input block
 //Have button functional, correspond to each input field
-  //Have button save data to local storage
-
+//Have button save data to local storage
 
 //Variable that shows today's date and time:
 let today = moment();
@@ -12,7 +11,7 @@ $("#currentDay").text(`Today's current date and time: ${today}`);
 // let start = "2010-10-15";
 // let result = moment(start).fromNow();
 // console.log(result);
-$(document).ready(function(){
+$(document).ready(function() {
   //Variable row is a new, empty div
   let row = $("<div>");
   row.attr("class", "row");
@@ -22,34 +21,38 @@ $(document).ready(function(){
   //Need to parse that if we want to be able to work with numbers
   currentHour = parseInt(currentHour);
   //For loop to dynamically create columns of numbers, input fields, and save buttons
-  for(let i=0; i<9; i++){
+  for (let i = 0; i < 9; i++) {
     //Array of hours we need to work with
-    let time = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+    let time = [9, 10, 11, 12, 13, 14, 15, 16, 17];
     //timeDiv is new, empty div where all text content will be appended to
     let timeDiv = $("<div>");
     timeDiv.addClass("hour col-3 time-block");
-      if(8<time[i] && time[i]<12){
-        timeDiv.text(`${time[i]}:00AM`);
-      } else{
-        timeDiv.text(`${time[i]}:00PM`);
-      }
-    // timeDiv.text(time[i]);
+    //if statement to dynamically add AM or PM based on numbers
+    timeDiv.text(`${time[i]}:00`);
     row.append(timeDiv);
-    let input = $("<input>");
-    input.addClass("col-8");
-    if(currentHour<6){
-      input.addClass("future");
+    let inputTag = $("<input>");
+    inputTag.addClass("col-8");
+    //if statement to dynamically change the color of the input field by adding class names
+    currentHour = 11;
+    if (time[i] < currentHour) {
+      inputTag.addClass("past");
+    } else if (time[i] === currentHour) {
+      inputTag.addClass("present");
     } else {
-      input.addClass("past");
+      inputTag.addClass("future");
     }
-    row.append(input);
+    row.append(inputTag);
+    //Creates save button and appends it to row
     let saveBtn = $("<button>");
     saveBtn.text("Save");
     saveBtn.attr("class", "saveBtn col-1");
     row.append(saveBtn);
-    saveBtn.on("click", function(){
-  
+    let tasks;
+    saveBtn.on("click", function() {
+      //Need these lines but they're wrong, some iteration of them...
+      // let userInput = inputTag.val();
+      // tasks = userInput;
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     });
-    
   }
-})
+});
