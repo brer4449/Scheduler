8c8d8d48dc97c9eec584ac0476b8cc35a9fb3d4b
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-14 19:14:59 -0500

    Fully functional app!

--- JS/app.js
+++ JS/app.js
@@ -3,7 +3,6 @@ $("#currentDay").text(`Today's current date and time: ${today}`);
 let currentHour = moment().format("H");
 //Need to parse that if we want to be able to work with numbers
 currentHour = parseInt(currentHour);
-let task;
 $(document).ready(function () {
   let row = $("<div>");
   row.addClass("row");
@@ -15,9 +14,14 @@ $(document).ready(function () {
       let timeDiv = $("<div>");
       timeDiv.addClass("hour col-3 time-block").text(`${time[i]}:00`);
       row.append(timeDiv);
+
       let inputTag = $("<input>");
+      //setting attribute of data to be the current interation of i (numbers 0-8)
       inputTag.attr("data", `${i}`).addClass("col-8");
+      let tasks = localStorage.getItem(`tasks${i}`);
+      inputTag.val(tasks)
       row.append(inputTag);
+
       //if statement to dynamically change the color of the input field by adding class names
       if (time[i] < currentHour) {
         inputTag.addClass("past");
@@ -26,19 +30,18 @@ $(document).ready(function () {
       } else {
         inputTag.addClass("future");
       }
+
       let saveBtn = $("<button>");
+      //setting attribute of data to be the current interation of i (numbers 0-8)
       saveBtn.text("Save").attr("data", `${i}`).addClass("saveBtn col-1");
       row.append(saveBtn);
+
       //Event listener for save button clicks. Setting and getting local storage
       saveBtn.on("click", function () {
+        //if save button data index matches inputTag data index
         if (saveBtn.attr(`data${i}`) == inputTag.attr(`data${i}`)) {
-          let userInput = inputTag.val()
-          let tasks = JSON.parse(localStorage.getItem(`tasks${i}`, $(this).attr("data")));
-          if (!tasks) {
-            tasks = [];
-          }
-          tasks.push(userInput);
-          localStorage.setItem(`tasks${i}`, JSON.stringify(tasks))
+          let userInput = inputTag.val();
+          localStorage.setItem(`tasks${i}`, userInput);
         }
       });
     };
